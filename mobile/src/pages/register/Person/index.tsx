48470ff62b4c51ba19777381262a758522731341
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-11-13 12:43:41 -0300

    :wrench: config registes screens

---
+++ mobile/src/pages/register/Person/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Person: React.FC = () => (
+  <View>
+    <Text>Person</Text>
+  </View>
+);
+
+export default Person;
---
+++ mobile/src/pages/register/Startup/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Startup: React.FC = () => (
+  <View>
+    <Text>Startup</Text>
+  </View>
+);
+
+export default Startup;
